03665b2c5fd2e08fa6d6fe202ec4b77f276bb247
Madhuri <madhuri.kattekola@innovacx.com>
2025-06-30 20:01:28 +0530

    added old version json-server

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
+
 
 server.use(middlewares);
 server.use(router);
+server.listen(3000, () => {
+  console.log('JSON Server is running on port 3000');
+});
 
 server.listen(port);
